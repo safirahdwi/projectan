27980107f22196eb05cc47e9f554759fbd1f93c3
monhack <fikri_alwahid@apps.ipb.ac.id>
2018-05-19 18:28:54 +0700

    Ini Tampilan sebelum login dan setelah login, tinggal masukin database maka akan jalan

---
+++ Tampilan Sebelum dan Sesudah Login/login.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<title>Smart PPKU</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<div class="form">
+    <img src="img/user.jpg" class="avatar">
+    <center><span class="welcome">Masuk</span></center>
+    <br>
+ <form action="login2.php" method="post">
+         <input class="input" type="text" name="username" placeholder="Username">
+         <input class="input" type="password" name="password" placeholder="Password">
+     <br>
+ <center><input class="submit" type="submit" value="Masuk" name="login"></center> 
+    </form>
+        <a href="#">Lupa Password?</a>
+        <a href="register.php" class="submit">Daftar</a><br>
+    </div>
+    </body>
+    </head>
+</html>
